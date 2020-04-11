1210964a28b3bd2686c7ed9f093beedfa8ca961b
asseysanzen <asseysanzen@gmail.com>
2020-04-12 02:25:01 +0900

    commit 4/11(3)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,24 @@ class BooksController < ApplicationController
 
   def index
   	@books = Book.all
-    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  def destroy
+    book = Book.find(params[:id])
+    list.destroy
+    redirect_to '/books'
   end
 
   private
