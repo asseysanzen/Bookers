87772370114c3d3948c308904aadcad7a410c44e
asseysanzen <asseysanzen@gmail.com>
2020-04-13 21:29:07 +0900

    commit 4/13(3)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,13 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Book.new(book_params)
-    book.save
-  	redirect_to book_path(book.id), flash:{create: "Book was successfully create!"}
+  	@book = Book.new(book_params)
+  if @book.save
+  	 redirect_to book_path(@book.id), flash:{create: "Book was successfully create!"}
+  else
+     @books = Book.all
+     render :index
+  end
   end
 
   def index
