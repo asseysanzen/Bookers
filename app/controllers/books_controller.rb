cddf8af4e3bcd326c162bb847452b54eb83a2aa0
asseysanzen <asseysanzen@gmail.com>
2020-04-14 14:33:55 +0900

    [finish] 5-8課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,9 +25,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id), flash:{update: "Book was successfully update!"}
+    @book = Book.find(params[:id])
+  if @book.update(book_params)
+    redirect_to book_path(@book.id), flash:{update: "Book was successfully update!"}
+  else
+      render :edit
+  end
   end
   def destroy
     book = Book.find(params[:id])
